fc4bc7e7d9b250e9df1f4c18ba63622d127caf62
“talhaumer” <talhumer7@gmail.com>
2022-04-20 17:58:33 +0500

    refactoring

--- dependencies.sh
+++ dependencies.sh
@@ -1,24 +1,24 @@
 python manage.py migrate
-python add_countries.py
-python add_perceval_driver.py
-python add_ratings.py
-python add_reason.py
-python approch.py
-python audience.py
-python constant_status.py
-python event_type.py
-python experties.py
-python languge.py
-python master_proctorship_constant.py
-python proctor_ship_contant.py
-python region.py
-python roles.py
-python solution.py
-python speaker_role.py
-python speciality.py
-python add_products.py
-python add_zones.py
-python add_hospitals.py
-python add_proctors.py
-python all_countries_zone.py
-python manage.py superuser
+python seeding-scripts/add_countries.py
+python seeding-scripts/add_perceval_driver.py
+python seeding-scripts/add_ratings.py
+python seeding-scripts/add_reason.py
+python seeding-scripts/approch.py
+python seeding-scripts/audience.py
+python seeding-scripts/constant_status.py
+python seeding-scripts/event_type.py
+python seeding-scripts/experties.py
+python seeding-scripts/languge.py
+python seeding-scripts/master_proctorship_constant.py
+python seeding-scripts/proctor_ship_contant.py
+python seeding-scripts/region.py
+python seeding-scripts/roles.py
+python seeding-scripts/solution.py
+python seeding-scripts/speaker_role.py
+python seeding-scripts/speciality.py
+python seeding-scripts/add_products.py
+python seeding-scripts/add_zones.py
+python seeding-scripts/add_hospitals.py
+python seeding-scripts/add_proctors.py
+python seeding-scripts/all_countries_zone.py
+python seeding-scripts/manage.py superuser
